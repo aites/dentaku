685265a494d167d0f4ec84d6651d7a1bee94fdd8
p_araki_aguri01 <a_araki@waku-2.com>
2018-11-19 20:58:31 +0900

    繰り返しの書き方

--- my-project/src/components/myheader.vue
+++ my-project/src/components/myheader.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    ここはヘッダーです。
+    <p>
+      ここはヘッダーです。
+    </p>
+    <ul>
+    <li v-for="item in 10" v-bind:key="item">
+    <p>{{ item }} は {{ item }}円</p>
+    </li>
+    </ul>
   </div>
 </template>
