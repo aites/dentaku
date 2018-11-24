b46e24b95031a0d9d53a2a0bd4aa5121f6511dc3
a.aguri <>
2018-11-25 02:20:09 +0900

    電卓機能（仮）の実装済み

--- my-project/src/components/dentaku.vue
+++ my-project/src/components/dentaku.vue
@@ -3,17 +3,23 @@
     <p class="dentaku-title">
       電卓です
     </p>
-    <div class="dentaku-board">
-    <button v-for="item in 9" v-bind:key="item" class="dentaku-board-button" v-bind:id="item" v-on:click="clickNumber(item)">{{ item }}</button>
-    <button class="dentaku-board-button" v-on:click="clickOperator('+')">+</button>
-    <button class="dentaku-board-button" v-on:click="clickOperator('-')">-</button>
-    <button class="dentaku-board-button" v-on:click="clickOperator('*')">*</button>
-    <button class="dentaku-board-button" v-on:click="clickOperator('/')">/</button>
-    <button class="dentaku-board-button" v-on:click="clickResult('=')">=</button>
+    <div>
+      計算結果:<span id="result">{{counter}}</span>
     </div>
-    <div id="">
-      計算結果<input type="number" >
-      <p id="result">{{counter}}</P>
+    <div class="dentaku-board">
+      <div class="dentaku-board-number">
+        <button v-for="item in 9" v-bind:key="item" class="dentaku-board-button" v-bind:id="item" v-on:click="clickNumber(item)">{{ item }}</button>
+        <button class="dentaku-board-button dentaku-board-button__w2" v-bind:id="item" v-on:click="clickNumber(0)">{{ 0 }}</button>
+        <button class="dentaku-board-button" v-on:click="clickNumber('.')">.</button>
+      </div>
+      <div class="dentaku-board-otg">
+        <button class="dentaku-board-button" v-on:click="clickClear()">C</button>
+        <button class="dentaku-board-button" v-on:click="clickOperator('+')">+</button>
+        <button class="dentaku-board-button" v-on:click="clickOperator('-')">-</button>
+        <button class="dentaku-board-button" v-on:click="clickOperator('*')">*</button>
+        <button class="dentaku-board-button" v-on:click="clickOperator('/')">/</button>
+        <button class="dentaku-board-button dentaku-board-button__w2" v-on:click="clickResult('=')">=</button>
+      </div>
     </div>
   </div>
 </template>
@@ -34,27 +40,93 @@ export default {
     return {
       counter: 0,
       operator: '',
-      beforeNum: 0
+      beforeNum: 0,
+      equal: '',
+      formula: ''
     }
   },
   methods: {
-        clickNumber: function (item) {
-            if (this.operator === '' || this.counter === 0) {
-              this.counter = ''
-            }
-            this.counter += String(item)
-        },
-        clickOperator: function (operator) {
-          this.operator = operator
-          this.beforeNum = this.counter
-          this.counter = 0
-        },
-        clickResult: function () {
-          if (this.operator !== '') {
-            this.counter = eval(this.beforeNum + this.operator + this.counter)
-            this.operator = ''
-          }
+    clickNumber: function (item) {
+      if (this.equal === '=' || this.counter === 0) {
+        this.counter = ''
+        this.equal = ''
+        this.formula = ''
+      }
+      if (item === '.') {
+        if (String(this.counter).indexOf('.') !== -1) {
+          item = ''
+        } else if (String(this.counter) === '') {
+          item = '0.'
         }
+      }
+      this.counter += String(item)
+    },
+    clickOperator: function (operator) {
+      this.operator = operator
+      this.beforeNum = this.counter
+      this.counter = 0
+    },
+    clickResult: function () {
+      if (this.operator !== '') {
+        if(this.formula === '') {
+          this.formula = this.operator + this.counter;
+        }
+        if(this.equal !== '=') {
+          this.counter = eval(this.beforeNum + this.formula)
+        }
+        else {
+          this.counter = eval(this.counter + this.formula)
+        }
+        this.equal = '='
+      }
+    },
+    clickClear: function () {
+      this.counter = 0
+      this.operator = ''
+      this.equal = ''
+      this.formula = ''
     }
+  }
 }
 </script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+button {
+  width: 32px;
+  height: 32px;
+  margin: 4px;
+}
+
+.dentaku-board-button__w2{
+  width: 72px;
+}
+
+.dentaku-board{
+  width: 240px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.dentaku-board-number{
+  width: 120px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.dentaku-board-otg{
+  width: 80px;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
